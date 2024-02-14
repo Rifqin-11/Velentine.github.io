32b3ab35fd9c5b78869bc99946941a091a960c34
Rifqi Naufal <rifqinaufal9009@gmail.com>
2024-02-14 22:07:53 +0700

    fix error

--- Script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('noButton').addEventListener('click', function() {
-    var yesButton = document.querySelector('button[onclick*="display-3.html"]');
-    var currentFontSize = parseInt(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = (currentFontSize + 10) + 'px'; // Increase font size by 5px
-});
